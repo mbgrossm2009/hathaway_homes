9893ba8460b66f98ca8097b5115fed98a7657626
Matthew Grossman <grossmanmatt@yahoo.com>
2017-09-17 18:34:31 -0400

    fixed bug in the edit template

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,7 +2,6 @@ class ListingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
   @listings = Listing.all
   end
 
@@ -11,28 +10,25 @@ class ListingsController < ApplicationController
   end
 
   def create
-
     @listing = Listing.new(listing_params)
     if @listing.save
       flash[:alert] = "New Listing Added Successfully"
       redirect_to listings_path
-
     else
       flash[:alert]= @listing.errors.full_messages
       render :new
+    end
   end
-end
 
-def destroy
-  @listing = Listing.find(params[:id])
-  @listing.destroy
-  flash[:alert] = "Listing has been deleted"
-  redirect_to listings_path
-end
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    flash[:alert] = "Listing has been deleted"
+    redirect_to listings_path
+  end
 
   def show
     @listing = Listing.find(params[:id])
-
   end
 
   def edit
@@ -42,7 +38,7 @@ end
   def update
   @listing = Listing.find(params[:id])
   if @listing.update_attributes(listing_params) && current_user == @listing.user
-    flash[:alert] = "Cell Phone Successfully Updated"
+    flash[:alert] = "Listing Successfully Updated"
     redirect_to listings_path
   else
     render 'edit'
@@ -50,7 +46,7 @@ end
 end
 private
 
-def listing_params
+  def listing_params
     params.require(:listing).permit(:house_style, :street_address, :state, :zipcode, :number_of_bathrooms, :number_of_bedrooms, :number_of_acres, :asking_price, :listing_photo, :town,:user_id)
   end
 end
