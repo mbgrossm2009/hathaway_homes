b750752c02ac220c1c31902894adb0fc342e937f
Matthew Grossman <grossmanmatt@yahoo.com>
2017-10-15 22:43:52 -0400

    added some css and minor ruby

--- app/uploaders/listing_photo_uploader.rb
+++ app/uploaders/listing_photo_uploader.rb
@@ -34,9 +34,9 @@ class ListingPhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fit => [400, 400]
-  end
+  # version :thumb do
+  #   process :resize_to_fit => [400, 400]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
