9a80427f8912f13cb6573e27df0aa5f210647c94
Matthew Grossman <grossmanmatt@yahoo.com>
2017-11-08 17:55:12 -0500

    css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create([{ first_name: 'David' }, {last_name: 'Hathaway'}, {email: 'test1@email.com'}, {phone_number: '555-555-5555'}, {password: 'password'}, {password_confirmation: 'password'}])
