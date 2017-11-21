90ae6162ae7e5d4f260ea57207b492e336904e89
Matthew Grossman <grossmanmatt@yahoo.com>
2017-11-21 16:42:57 -0500

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
    config.action_mailer.perform_deliveries = true
-   config.action_mailer.smtp_settings = {
+   ActionMailer::Base.smtp_settings = {
      :user_name => ENV['SENDGRID_USERNAME'],
      :password => ENV['SENDGRID_PASSWORD'],
-     :domain => 'yourdomain.com',
+     :domain => 'heroku.com',
      :address => 'smtp.sendgrid.net',
      :port => 587,
      :authentication => :plain,
