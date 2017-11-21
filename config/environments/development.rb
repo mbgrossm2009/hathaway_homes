df32573f4e5bd374dbf1d3fac0a4899c9fe864f8
Matthew Grossman <grossmanmatt@yahoo.com>
2017-11-21 16:46:07 -0500

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,20 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :smtp
-   config.action_mailer.perform_deliveries = true
-   ActionMailer::Base.smtp_settings = {
-     :user_name => ENV['SENDGRID_USERNAME'],
-     :password => ENV['SENDGRID_PASSWORD'],
-     :domain => 'heroku.com',
-     :address => 'smtp.sendgrid.net',
-     :port => 587,
-     :authentication => :plain,
-     :enable_starttls_auto => true
-   }
 
   config.action_mailer.perform_caching = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,19 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.delivery_method = :smtp
+   ActionMailer::Base.smtp_settings = {
+     :user_name => ENV['SENDGRID_USERNAME'],
+     :password => ENV['SENDGRID_PASSWORD'],
+     :domain => 'heroku.com',
+     :address => 'smtp.sendgrid.net',
+     :port => 587,
+     :authentication => :plain,
+     :enable_starttls_auto => true
+   }
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
