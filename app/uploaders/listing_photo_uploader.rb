ce10e3693ca06d7ec41a5ee431bf6625d597038d
Matthew Grossman <grossmanmatt@yahoo.com>
2017-12-03 23:48:37 -0500

    added amazon s3 to carrierwave

--- app/uploaders/listing_photo_uploader.rb
+++ app/uploaders/listing_photo_uploader.rb
@@ -8,7 +8,12 @@ class ListingPhotoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # if Rails.env.test?
+  if Rails.env.test?
     storage :file
+  else
+    storage :fog
+  end
+end
   # else
     # storage :fog
   # end
