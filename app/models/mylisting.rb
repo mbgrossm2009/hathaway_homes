4fa5f229b5e7d789fd7f8d8afecdf325ab53ee0f
Matt Grossman <grossmanmatt@Matts-MBP.fios-router.home>
2018-11-06 12:46:23 -0500

    added add-on

--- app/models/mylisting.rb
+++ app/models/mylisting.rb
@@ -1,6 +1,6 @@
 class Mylisting < ApplicationRecord
   belongs_to :user, optional: true
-  # mount_uploaders :listing_photo, ListingPhotoUploader
+  mount_uploaders :listing_photo, ListingPhotoUploader
 
   def self.search(search)
     where("house_style like ? OR street_address like ? OR state like ? OR town like ? ", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,11 @@ module HathawayHomes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    moesif_options = {
+      'application_id' => 'eyJhcHAiOiIxMzI6MTA1IiwidmVyIjoiMi4wIiwib3JnIjoiNTg2OjY4IiwiaWF0IjoxNTQxNDYyNDAwfQ.0fs360xIyZrVRHyn43DyGYMvfosKmLG0--M0_9dayuM'
+    }
+
+    config.middleware.use "MoesifRack::MoesifMiddleware", moesif_options
   end
 end
