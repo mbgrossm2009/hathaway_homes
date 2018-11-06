60dc5392fc243420da325ff8d4506168b2831ec7
Matt Grossman <grossmanmatt@Matts-MBP.fios-router.home>
2018-11-06 17:09:55 -0500

    fix issue with fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,29 +1,22 @@
 
 CarrierWave.configure do |config|
 
-  # Use local storage if in development or test
-  if Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.storage = :file
-    end
+
+if Rails.env.test? or Rails.env.development?
+  CarrierWave.configure do |config|
+    config.storage = :file
   end
-
-  # Use AWS storage if in production
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
+else
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region => ENV['AWS_S3_REGION']
+    }
+    config.storage = :fog
+    config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
+    config.asset_host = "#{ENV['AWS_S3_ASSET_URL']}/#{ENV['AWS_S3_BUCKET_NAME']}"
   end
-
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: ENV["S3_REGION"]
-  }
-  config.fog_directory  = ENV["S3_BUCKET"]
-
-  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-  #config.fog_public     = false                                  # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
