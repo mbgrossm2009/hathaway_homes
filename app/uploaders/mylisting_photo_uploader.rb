97f665100d33c4016510fa890023861e78104777
Matt Grossman <grossmanmatt@Matts-MBP.fios-router.home>
2018-11-06 17:04:18 -0500

    fixed aws

--- app/uploaders/mylisting_photo_uploader.rb
+++ app/uploaders/mylisting_photo_uploader.rb
@@ -8,11 +8,7 @@ class MylistingPhotoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  if Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,29 @@
+
 CarrierWave.configure do |config|
-  if !Rails.env.test?
-    config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ENV["S3_REGION"]
-    }
-    config.fog_directory  = ENV["S3_BUCKET"]
+
+  # Use local storage if in development or test
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
   end
+
+  # Use AWS storage if in production
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+
+  config.fog_credentials = {
+    provider: "AWS",
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: ENV["S3_REGION"]
+  }
+  config.fog_directory  = ENV["S3_BUCKET"]
+
+  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+  #config.fog_public     = false                                  # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
