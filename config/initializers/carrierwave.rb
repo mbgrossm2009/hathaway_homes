5b45fbfe0e27983c66ef850f79439ea51dd6cbdb
Matt Grossman <grossmanmatt@Matts-MBP.fios-router.home>
2018-11-08 00:08:12 -0500

    issues with fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,6 @@
+require 'carrierwave/storage/fog'
+
+
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage = :fog
@@ -12,6 +15,7 @@ if Rails.env.production?
     config.cache_dir = 'files'
     config.permissions = 0777
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    config.storage = :fog
   end
 else
   CarrierWave.configure do |config|
