9dfc226ad52568b405dcaecdbd176e40f3029a12
Matt Grossman <grossmanmatt@Matts-MBP.fios-router.home>
2018-11-08 18:03:19 -0500

    aws fixes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,16 +5,13 @@ CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
   else
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['S3_REGION'],
-      endpoint: 'https://s3.amazonaws.com'
-    }
-    config.fog_directory = ENV['S3_BUCKET']
-    config.fog_public = false
-    config.storage = :fog
-  end
+      config.fog_credentials = {
+        :provider              => 'AWS',
+        :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+
+      config.fog_directory =  ENV['S3_BUCKET_PRODUCTION']
+      config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
+    end
 end
