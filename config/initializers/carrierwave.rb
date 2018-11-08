d4eaa8786be49ab191b6bdfdbf61b71142be3616
Matt Grossman <grossmanmatt@Matts-MBP.fios-router.home>
2018-11-08 18:09:17 -0500

    aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: ENV['S3_REGION'],
       endpoint: 'https://s3.amazonaws.com'
     }
