9c81ff86d42328c421aee32bde1ca010d12a3f4e
Matt Grossman <grossmanmatt@Matts-MBP.fios-router.home>
2018-11-14 23:29:44 -0500

    navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :load_messages
 
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 protected
+
+
+def load_messages
+    @message = Message.new
+end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :email])
   end
 
-  def load_messages
-      @message = Message.new
-  end
+
 
 
 
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,8 +1,12 @@
 class ListingsController < ApplicationController
 
+
+
+
   def index
     @zpids = Zpid.all
   end
+
 
 
 end
